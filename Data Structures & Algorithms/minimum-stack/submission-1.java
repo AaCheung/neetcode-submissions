632a8e8c2a464703476e2ddac496136bfc66aa3b
Aaron Cheung <38768350+AaCheung@users.noreply.github.com>
2026-08-03 20:52:06 -0400

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.java
@@ -0,0 +1,79 @@
+class Node {
+    int value, min;
+    Node prev, next, last;
+
+    public Node( int _v, Node _prev ) {
+        value = _v;
+        prev = _prev;
+        min = value;
+        if ( prev != null ) { 
+            prev.next = this;
+            min = Math.min( min, prev.min );
+        }
+    }
+
+    public void push( int _v ) {
+        if ( next != null ) next.push( _v );
+        else next = new Node( _v, this );
+    }
+
+    public Node last() {
+        if ( last != null && last != this ) return last.last();
+        last = ( next == null ? this : next.last() );
+        return last;
+    }
+
+    public void pop() {
+        if ( next.next == null ) {
+            next = null;
+        } else {
+            next.pop();
+        }
+    }
+
+    public int top() {
+        return last().value;
+    }
+
+    public int getMin() {
+        return min;
+    }
+
+    public String toString() {
+        return String.format( "( %d, %s )", value, next );
+    }
+
+}
+
+class MinStack {
+
+    Node node;
+    Node first;
+
+    ArrayList<Integer> list = new ArrayList<Integer>();
+
+    public MinStack() {
+        
+    }
+    
+    public void push(int val) {
+        node = new Node( val, node );
+        if ( node.prev == null ) first = node;
+        //System.out.println( first );
+    }
+    
+    public void pop() {
+        if ( node.prev != null ) { 
+            node = node.prev; 
+            node.pop();
+        } else node = null;
+    }
+    
+    public int top() {
+        return node.top();
+    }
+    
+    public int getMin() {
+        return node.getMin();
+    }
+}
